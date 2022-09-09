4574d65194f7f6e67d0b12320de2d259f54f5105
Kausar Alam <kausar.alm@gmail.com>
2022-09-09 16:26:07 +0600

    hover color
    Added Icon hover color pickker in js file

--- assets/js/colorPicker.js
+++ assets/js/colorPicker.js
@@ -2,5 +2,6 @@
   $(function () {
     // Add Color Picker to all inputs that have 'color-field' class
     $(".cpa-color-picker").wpColorPicker();
+    $(".cpa-color-picker-hover").wpColorPicker();
   });
 })(jQuery);
